40529d51c39cec8f012e6ac77b9327932f445f9f
skLuan <luan.erazo@skyrocket.com.co>
2024-10-20 01:06:25 -0500

    added the shader and material for the gem

--- Assets/Scripts/GemPart.cs
+++ Assets/Scripts/GemPart.cs
@@ -3,19 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GemPart : MonoBehaviour
-{   
+{
+    // Variable para el Mesh del hijo
+    public Transform gemPartMesh;
+
+    // Velocidades de rotación aleatoria
+    private Vector3 randomRotationSpeed;
     UIController uIController;
     // Start is called before the first frame update
     void Start()
-    {
-    //gameObject = (GameObject) this;
-    uIController = GameObject.Find("Ui").GetComponent<UIController>();
+    {   
+        gemPartMesh = gameObject.GetComponent<GemPart>().transform;
+        randomRotationSpeed = new Vector3(
+           0, // Rotación aleatoria en el eje X
+           Random.Range(-100f, 100f), // Rotación aleatoria en el eje Y
+           0  // Rotación aleatoria en el eje Z
+       );
+        //gameObject = (GameObject) this;
+        uIController = GameObject.Find("Ui").GetComponent<UIController>();
     }
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other)
+    {
         if (other.CompareTag("Player"))
-        {   
+        {
             uIController.IncreaseScore();
             gameObject.SetActive(false);
         }
+    }
+
+    void Update()
+    {
+        // Rotar el hijo GemPartMesh cada frame
+        gemPartMesh.Rotate(randomRotationSpeed * Time.deltaTime);
     }
 }
